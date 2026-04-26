5c8c1339c7b1a9537405079856ee14761e561fbd
Reviusion <vladbudaev82@gmail.com>
2026-04-26 17:05:44 +0300

    Delete src directory

--- src/boost_allow_tearing.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace boost_allow_tearing {
-    void apply();
-    bool isEnabled();           // true if ALLOW_TEARING flag is being injected
-    bool isTearingSupported();  // DXGI 1.5 + driver actually supports it
-}
